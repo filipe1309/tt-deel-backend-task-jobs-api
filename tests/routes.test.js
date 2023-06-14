ec6a154ceab26045219394dcbe2e3ad367cf59ae
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-13 21:31:06 -0300

    test: deposits money into the the balance of a client

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,7 +2,12 @@ const request = require('supertest')
 const app = require('../src/app')
 
 
-describe('Get Endpoints', () => {
+beforeAll(async () => {
+    // await require('../src/model').sequelize.sync()
+    jest.setTimeout(10000)
+})
+
+describe('Contracts Endpoints', () => {
 
     it('should return 401 if profile_id header is not set', async () => {
         const res = await request(app)
@@ -26,7 +31,9 @@ describe('Get Endpoints', () => {
         expect(res.statusCode).toEqual(200)
         expect(res.body.length).toBeGreaterThan(0)
     })
+})
 
+describe('Jobs Endpoints', () => {
     it('should return a list of unpaid jobs', async () => {
         const res = await request(app)
             .get('/jobs/unpaid')
@@ -35,15 +42,24 @@ describe('Get Endpoints', () => {
         expect(res.body.length).toBe(1)
         expect(res.body[0].Contract.status).toBe("in_progress")
     })
-})
 
-describe('Post Endpoints', () => {
     it('should pay for a job', async () => {
         const res = await request(app)
             .post('/jobs/1/pay')
             .set('profile_id', 1)
         expect(res.statusCode).toEqual(200)
         expect(res.body.paid).toBe(true)
+    })
+
+    it('should deposit money into the balance of a client', async () => {
+        // timeout
+        jest.setTimeout(10000)
+        const res = await request(app)
+            .post('/balances/deposit/1')
+            .set('profile_id', 1)
+            .send({ amount: 50 })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.balance).toBe(1000)
     })
 })
 
