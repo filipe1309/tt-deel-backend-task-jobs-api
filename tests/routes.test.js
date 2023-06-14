5111c206b26531655179a7bcba45f91c8a5b006d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-13 22:22:58 -0300

    test: returns the clients the paid the most for jobs in the query time period

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -64,6 +64,16 @@ describe('Admin Endpoints', () => {
         expect(res.body.profession).toBe("Programmer")
         expect(res.body.total).toBe(2683)
     })
+
+    it('should return the best clients', async () => {
+        const res = await request(app)
+            .get('/admin/best-clients?start=2020-08-14T23:11:26.737Z&end=2020-08-17T19:11:26.737Z&limit=2')
+            .set('profile_id', 1)
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.length).toBe(2)
+        expect(res.body[0].fullName).toBe("Ash Kethcum")
+        expect(res.body[0].paid).toBe(2020)
+    })
 });
 
 
